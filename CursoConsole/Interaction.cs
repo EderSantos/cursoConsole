951d113bc219be19066c11f60fd6cb70be8880cf
EderSantos <edeilsonsantos89@gmail.com>
2016-07-11 21:54:03 -0300

    cursoConsole

---
+++ CursoConsole/Interaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CursoConsole
+{
+    class Interaction
+    {
+        internal static void MsgBox(string p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
